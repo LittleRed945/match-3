caba22048881d37d77ebca47be37550c28b4531d
QB <usengjake@outlook.com>
2022-04-27 11:44:55 +0800

    Initial Commit

---
+++ match 3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  match 3
+//
+//  Created by User02 on 2022/4/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ match 3/match_3App.swift
@@ -0,0 +1,17 @@
+//
+//  match_3App.swift
+//  match 3
+//
+//  Created by User02 on 2022/4/27.
+//
+
+import SwiftUI
+
+@main
+struct match_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
