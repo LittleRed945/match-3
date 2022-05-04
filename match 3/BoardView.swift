382b5d5c0ce2f8981b5f161ec6cff565659cfaf2
QB <usengjake@outlook.com>
2022-05-04 11:19:51 +0800

    D

--- match 3/BoardView.swift
+++ match 3/BoardView.swift
@@ -2,14 +2,38 @@ import Foundation
 import SwiftUI
 struct BoardView: View {
     @StateObject var game=game_variable()
+    @State private var startDetectDrag = false
+    @State private var number = 1
+    var dragGesture: some Gesture {
+        DragGesture(minimumDistance: 0)
+            .onChanged({ value in
+            if startDetectDrag {
+            if value.translation.width > 5 {
+            number += 1
+                print(value.translation.width)
+            startDetectDrag = false
+
+            } else if value.translation.width < -5 {
+            number -= 1
+
+            startDetectDrag = false
+
+            }
+            } else {
+            if value.translation == .zero {
+            startDetectDrag = true
+            }
+            }
+            })
+    }
     var body: some View {
-        VStack{
+        VStack(spacing:0){
             ForEach(game.board.indices,id:\.self){y in
-                HStack{
+                HStack(spacing:0){
                     ForEach(game.board[y].indices,id:\.self){x in
                         ZStack{
                             Rectangle().stroke().frame(width: 30, height: 30)
-                            Image("\(game.board[y][x].img_id)")
+                            Image("\(game.board[y][x].img_id)").gesture(dragGesture)
                         }
                         
                     }
